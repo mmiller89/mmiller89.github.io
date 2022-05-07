f7267adcbb1cde9f9a9170cfaf9d6c3a7e374765
Michael Miller <mchlmiller89@gmail.com>
2022-05-06 18:38:51 -0700

    Added functionality of game, can check words.

--- src/components/Boggle/Boggle.js
+++ src/components/Boggle/Boggle.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './boggle.css'
 import { useState } from 'react';
+import dictionaryArray from './dictionary.js';
 
 
 //TO-DOs
-//Build algorithm to identify if letters in accepted word are all connected.
-//Import word library.
- 
+//Clicking on the same square uncolors it and removes the letter from the input.
+//Add sound effects.
+//Add score system: 3/4 letter words - 1 pt, 5 letters - 2 pts, 6 = 3 pts, 7 = 5 pts, 8+ = 11 pts
+//Find Boggle word library for dictionary.js 
+
+var letterTracker = []
+var wordsCorrect = 0;
 
 const Boggle = () => {
+    
     let currentBoard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     let letters = [
             ["R","I","F","O","B","X"],
@@ -45,6 +51,12 @@ const Boggle = () => {
     const [thirteenColor, setThirteenColor] = useState("")
     const [fourteenColor, setFourteenColor] = useState("")
     const [fifteenColor, setFifteenColor] = useState("")
+    const [correctCol, setCorrectCol] = useState([
+        {
+            text: "", 
+            color: ""
+        }
+    ]);
     const [placeHold, setPlaceHold] = useState("")
     
     
@@ -65,7 +77,7 @@ const Boggle = () => {
     const [slots13, setSlots13] = useState("");
     const [slots14, setSlots14] = useState("");
     const [slots15, setSlots15] = useState("");
-    const [inputValue, setInputValue] = useState("Boggle!!!")
+    const [inputValue, setInputValue] = useState("")
     const [shuffleButton, setShuffleButton] = useState("Shuffle")
     
 
@@ -73,7 +85,13 @@ const Boggle = () => {
     
     
     const shuffleBegin = () => {
-            var timerMinutes = 60 * 1
+        wordsCorrect = 0;
+        setCorrectCol({
+            ...correctCol,
+            text: 'Good luck!',
+            color: 'black'
+        })
+            var timerMinutes = 60 * 3
             shuffleLetters();
             setBoard();
             setShuffleButton(timer(timerMinutes))
@@ -143,56 +161,125 @@ const Boggle = () => {
                 resetColors();
                 timer = duration;
                 clearInterval(timerCount)
-                setInputValue("Boggle!!!")
+                setInputValue("")
                 setShuffleButton("Shuffle")
+                setCorrectCol({
+                    ...correctCol,
+                    text: `You got ${wordsCorrect} words correct`,
+                    color: 'black'
+                })
             }
         }, 1000);
     }
 
-    const addLetter = (slotVal, slotClicked) => {
-        if (inputValue === "Boggle!!!"){
+    const letterCheck = (slotVal) => {
+        
+        if (inputValue === ""){
             setInputValue(slotVal)
+            
         } else {
             setInputValue(inputValue + slotVal)
-        }
-        tdColorCheck(slotClicked)
             
+        }    
     }
 
-    const tdColorCheck = (slotClicked) => {
-        slotClicked === 0 && shuffleButton !== "Shuffle" ? setZeroColor('green') : setPlaceHold('')
-        slotClicked === 1 && shuffleButton !== "Shuffle" ? setOneColor('green') :  setPlaceHold('')
-        slotClicked === 2 && shuffleButton !== "Shuffle" ? setTwoColor('green') :  setPlaceHold('')
-        slotClicked === 3 && shuffleButton !== "Shuffle" ? setThreeColor('green') :  setPlaceHold('')
-        slotClicked === 4 && shuffleButton !== "Shuffle" ? setFourColor('green') :  setPlaceHold('')
-        slotClicked === 5 && shuffleButton !== "Shuffle" ? setFiveColor('green') :  setPlaceHold('')
-        slotClicked === 6 && shuffleButton !== "Shuffle" ? setSixColor('green') :  setPlaceHold('')
-        slotClicked === 7 && shuffleButton !== "Shuffle" ? setSevenColor('green') :  setPlaceHold('')
-        slotClicked === 8 && shuffleButton !== "Shuffle" ? setEightColor('green') :  setPlaceHold('')
-        slotClicked === 9 && shuffleButton !== "Shuffle" ? setNineColor('green') :  setPlaceHold('')
-        slotClicked === 10 && shuffleButton !== "Shuffle" ? setTenColor('green') :  setPlaceHold('')
-        slotClicked === 11 && shuffleButton !== "Shuffle" ? setElevenColor('green') :  setPlaceHold('')
-        slotClicked === 12 && shuffleButton !== "Shuffle" ? setTwelveColor('green') :  setPlaceHold('')
-        slotClicked === 13 && shuffleButton !== "Shuffle" ? setThirteenColor('green') :  setPlaceHold('')
-        slotClicked === 14 && shuffleButton !== "Shuffle" ? setFourteenColor('green') :  setPlaceHold('')
-        slotClicked === 15 && shuffleButton !== "Shuffle" ? setFifteenColor('green') :  setPlaceHold('')
+    const tdColorCheck = (slotVal, slotClicked) => {
+        letterTracker.push(slotClicked);
+        console.log(letterTracker);
+        let validConnection = isCorrectWord(slotClicked);
+        console.log(validConnection)
+        slotClicked === 0 && shuffleButton !== "Shuffle" && validConnection === true ? setZeroColor('green') : setPlaceHold('')
+        slotClicked === 1 && shuffleButton !== "Shuffle" && validConnection === true ? setOneColor('green') :  setPlaceHold('')
+        slotClicked === 2 && shuffleButton !== "Shuffle" && validConnection === true ? setTwoColor('green') :  setPlaceHold('')
+        slotClicked === 3 && shuffleButton !== "Shuffle" && validConnection === true ? setThreeColor('green') :  setPlaceHold('')
+        slotClicked === 4 && shuffleButton !== "Shuffle" && validConnection === true ? setFourColor('green') :  setPlaceHold('')
+        slotClicked === 5 && shuffleButton !== "Shuffle" && validConnection === true ? setFiveColor('green') :  setPlaceHold('')
+        slotClicked === 6 && shuffleButton !== "Shuffle" && validConnection === true ? setSixColor('green') :  setPlaceHold('')
+        slotClicked === 7 && shuffleButton !== "Shuffle" && validConnection === true ? setSevenColor('green') :  setPlaceHold('')
+        slotClicked === 8 && shuffleButton !== "Shuffle" && validConnection === true ? setEightColor('green') :  setPlaceHold('')
+        slotClicked === 9 && shuffleButton !== "Shuffle" && validConnection === true ? setNineColor('green') :  setPlaceHold('')
+        slotClicked === 10 && shuffleButton !== "Shuffle" && validConnection === true ? setTenColor('green') :  setPlaceHold('')
+        slotClicked === 11 && shuffleButton !== "Shuffle" && validConnection === true? setElevenColor('green') :  setPlaceHold('')
+        slotClicked === 12 && shuffleButton !== "Shuffle" && validConnection === true ? setTwelveColor('green') :  setPlaceHold('')
+        slotClicked === 13 && shuffleButton !== "Shuffle" && validConnection === true ? setThirteenColor('green') :  setPlaceHold('')
+        slotClicked === 14 && shuffleButton !== "Shuffle" && validConnection === true ? setFourteenColor('green') :  setPlaceHold('')
+        slotClicked === 15 && shuffleButton !== "Shuffle" && validConnection === true ? setFifteenColor('green') :  setPlaceHold('')
+        if (validConnection === true){
+            letterCheck(slotVal);
+        } else {
+            letterTracker.pop();
+        }
+        
     }
 
     const clearWord = () => {
-        setInputValue("Boggle!!!")
+        setInputValue("")
         resetColors();
     }
-    
-    const isCorrectWord = () => {
 
+    const checkWord = () => {
+        console.log(inputValue)
+        if (shuffleButton !== "Shuffle" && dictionaryArray.includes(inputValue.toLowerCase())){
+          setCorrectCol({
+              ...correctCol,
+              text: 'Correct!',
+              color: 'green'
+          })
+          wordsCorrect ++
+            
+        } else {
+            setCorrectCol({
+                ...correctCol,
+                text: 'Wrong!',
+                color: 'red'
+            })
+            
+        }
+      
+        setInputValue("")
+        resetColors()
+    }
+    
+    const isCorrectWord = (slotClicked) => {
+        const validConnections = [
+            [1, 4, 5], //index 0
+            [0, 4, 5, 6, 2], //index 1
+            [1, 3, 5, 6, 7], //index 2
+            [2, 6, 7], //index 3
+            [0, 1, 5, 8, 9], //index 4
+            [0, 1, 2, 4, 8, 9, 10, 6], //index 5
+            [1, 2, 3, 5, 9, 10, 11, 7], //index 6
+            [2, 3, 6, 11, 10], //index 7
+            [9, 4, 5, 12, 13], //index 8
+            [6, 4, 5,8,10,12,13, 14], //index 9
+            [5, 6, 7,9,11,13,14,15], //index 10
+            [6, 7, 10,14,15], //index 11
+            [8, 9, 13], //index 12
+            [8, 9, 10,12,14], //index 13
+            [9, 10, 11,13,15], //index 14
+            [10, 11, 14] //index 15
+        ]
+        if (inputValue === ""){
+            return true;
+        }
+        
+        if (validConnections[slotClicked].includes(letterTracker[letterTracker.length-2])){
+            return true;
+        } 
+        
+        return false;
+
+       
+       
     }
     
 
     return (
         <div>
+            <div style={{ color: `${correctCol.color}` }}>{correctCol.text}</div>
             <div className="card">
                 <div className='card-body'>
-                    {inputValue}
+                🤓{inputValue}
                 </div>
                 
             </div>
@@ -201,33 +288,33 @@ const Boggle = () => {
                     shuffleBegin();
                 }
             } }><span className="start-button">{shuffleButton}</span></button>
-            <button type="button" className="btn btn-lg btn-success"><span className="start-button">Check Word</span></button>
+            <button type="button" className="btn btn-lg btn-success" onClick={checkWord}><span className="start-button">Check Word</span></button>
             <button type="button" className="btn btn-lg btn-success" onClick={clearWord}><span className="start-button">Clear</span></button>
             <table className="table table-light table-bordered">
                 <tbody>
                     <tr>
-                        <td style ={{backgroundColor: `${zeroColor}`}} onClick={() => addLetter(slots0, 0)}><div>{slots0}</div></td>
-                        <td style ={{backgroundColor: `${oneColor}`}} onClick={() => addLetter(slots1, 1)}><div>{slots1}</div></td>
-                        <td style ={{backgroundColor: `${twoColor}`}} onClick={() => addLetter(slots2, 2)}><div>{slots2}</div></td>
-                        <td style ={{backgroundColor: `${threeColor}`}} onClick={() => addLetter(slots3, 3)}><div>{slots3}</div></td>
+                        <td style={{ backgroundColor: `${zeroColor}` }} onClick={() => tdColorCheck(slots0, 0, inputValue)}><div>{slots0}</div></td>
+                        <td style ={{backgroundColor: `${oneColor}`}} onClick={() => tdColorCheck(slots1, 1, inputValue)}><div>{slots1}</div></td>
+                        <td style ={{backgroundColor: `${twoColor}`}} onClick={() => tdColorCheck(slots2, 2, inputValue)}><div>{slots2}</div></td>
+                        <td style ={{backgroundColor: `${threeColor}`}} onClick={() => tdColorCheck(slots3, 3, inputValue)}><div>{slots3}</div></td>
                     </tr>
                     <tr>
-                        <td style ={{backgroundColor: `${fourColor}`}} onClick={() => addLetter(slots4, 4)}><div>{slots4}</div></td>
-                        <td style ={{backgroundColor: `${fiveColor}`}} onClick={() => addLetter(slots5, 5)}><div>{slots5}</div></td>
-                        <td style ={{backgroundColor: `${sixColor}`}} onClick={() => addLetter(slots6, 6)}><div>{slots6}</div></td>
-                        <td style ={{backgroundColor: `${sevenColor}`}} onClick={() => addLetter(slots7, 7)}><div>{slots7}</div></td>
+                        <td style ={{backgroundColor: `${fourColor}`}} onClick={() => tdColorCheck(slots4, 4, inputValue)}><div>{slots4}</div></td>
+                        <td style ={{backgroundColor: `${fiveColor}`}} onClick={() => tdColorCheck(slots5, 5, inputValue)}><div>{slots5}</div></td>
+                        <td style ={{backgroundColor: `${sixColor}`}} onClick={() => tdColorCheck(slots6, 6, inputValue)}><div>{slots6}</div></td>
+                        <td style ={{backgroundColor: `${sevenColor}`}} onClick={() => tdColorCheck(slots7, 7, inputValue)}><div>{slots7}</div></td>
                     </tr>
                     <tr>
-                        <td style ={{backgroundColor: `${eightColor}`}} onClick={() => addLetter(slots8, 8)}><div>{slots8}</div></td>
-                        <td style ={{backgroundColor: `${nineColor}`}} onClick={() => addLetter(slots9, 9)}><div>{slots9}</div></td>
-                        <td style ={{backgroundColor: `${tenColor}`}} onClick={() => addLetter(slots10, 10)}><div>{slots10}</div></td>
-                        <td style ={{backgroundColor: `${elevenColor}`}} onClick={() => addLetter(slots11, 11)}><div>{slots11}</div></td>
+                        <td style ={{backgroundColor: `${eightColor}`}} onClick={() => tdColorCheck(slots8, 8, inputValue)}><div>{slots8}</div></td>
+                        <td style ={{backgroundColor: `${nineColor}`}} onClick={() => tdColorCheck(slots9, 9, inputValue)}><div>{slots9}</div></td>
+                        <td style ={{backgroundColor: `${tenColor}`}} onClick={() => tdColorCheck(slots10, 10, inputValue)}><div>{slots10}</div></td>
+                        <td style ={{backgroundColor: `${elevenColor}`}} onClick={() => tdColorCheck(slots11, 11, inputValue)}><div>{slots11}</div></td>
                     </tr>
                     <tr>
-                        <td style ={{backgroundColor: `${twelveColor}`}} onClick={() => addLetter(slots12, 12)}><div>{slots12}</div></td>
-                        <td style ={{backgroundColor: `${thirteenColor}`}} onClick={() => addLetter(slots13, 13)}><div>{slots13}</div></td>
-                        <td style ={{backgroundColor: `${fourteenColor}`}} onClick={() => addLetter(slots14, 14)}><div>{slots14}</div></td>
-                        <td style ={{backgroundColor: `${fifteenColor}`}} onClick={() => addLetter(slots15, 15)}><div>{slots15}</div></td>
+                        <td style ={{backgroundColor: `${twelveColor}`}} onClick={() => tdColorCheck(slots12, 12, inputValue)}><div>{slots12}</div></td>
+                        <td style ={{backgroundColor: `${thirteenColor}`}} onClick={() => tdColorCheck(slots13, 13, inputValue)}><div>{slots13}</div></td>
+                        <td style ={{backgroundColor: `${fourteenColor}`}} onClick={() => tdColorCheck(slots14, 14, inputValue)}><div>{slots14}</div></td>
+                        <td style ={{backgroundColor: `${fifteenColor}`}} onClick={() => tdColorCheck(slots15, 15, inputValue)}><div>{slots15}</div></td>
                     </tr>
                 </tbody>
             </table>
--- src/components/Features.js
+++ src/components/Features.js
@@ -10,7 +10,7 @@ const Features = () => {
             <h1 className="centered">ReactJS Boggle</h1>
             <h3><strong>Directions</strong>: Click on Shuffle to begin. Click the letter squares to form a word.</h3>
             <h3>Each letter must be connected for the word to count.</h3>
-            <h3>Note: The game is not functional yet. I am just testing the styling.</h3>
+            
             <Boggle />
         </div>
     )
